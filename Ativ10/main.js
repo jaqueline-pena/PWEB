e2f51b5367e8c17e08d54a5075d6ed2607847465
jaqueline-pena <111199155+jaqueline-pena@users.noreply.github.com>
2023-10-09 12:07:51 -0300

    Add files via upload

---
+++ Ativ10/main.js
@@ -0,0 +1,9 @@
+const numeroMaior = encontrarMaiorNumero(73, 21, 9);
+
+document.getElementById("maiorNumero").textContent = numeroMaior;
+
+ 
+
+const numerosOrdenados = ordenarNumerosCrescente(73, 21, 9);
+
+document.getElementById("numerosOrdenados").textContent = numerosOrdenados.join(", ");
